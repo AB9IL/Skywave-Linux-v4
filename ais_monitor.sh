a96663ea78d2cde22e656d6e901488e696ba183e
Philip Colllier <redcrown01@gmail.com>
2020-07-12 06:22:44 +0000

    Update ais_monitor.sh
    Updated mapping function, named pipes.

--- ais_monitor.sh
+++ ais_monitor.sh
@@ -14,10 +14,16 @@ jsonfile=$HOME"/ais_log.json"
 ppm=$(cat /usr/local/etc/sdr_offset)
 #Get the SDR gain (gain)
 gain=$(cat /usr/local/etc/sdr_gain)
+fifo='/tmp/ais.dump'
 
 dumpto_file() {
+# create a pipe
+mkfifo -m 666 $fifo
+# start reading from the pipe
+cat $fifo | gpsdecode -j | tee -a $jsonfile &
+# feed the pipe
 rtl_ais -p $ppm -g $gain &
-nc 127.0.0.1 10110 | gpsdecode -j | tee -a $jsonfile &
+nc 127.0.0.1 10110 > $fifo
 
 WINDOW=$(zenity --info --height 100 --width 350 \
 --title="RTL-AIS - Running." \
@@ -27,10 +33,13 @@ To stop, use this application and select \"Stop AIS capture and logging.\""
 }
 
 dumpto_db(){
-# open a database session, naming the db file
-# get nmea data and pipe it through the converer to get json data
+# create a pipe
+mkfifo -m 666 $fifo
+# call the python script to read the pipe and build the database
+sh -c "ais-fileto-sqlite $fifo"
+# get nmea data and feed the pipe
 rtl_ais -p $ppm -g $gain &
-nc 127.0.0.1 10110 | gpsdecode -j | ais-capturestream
+nc 127.0.0.1 10110 > $fifo
 
 WINDOW=$(zenity --info --height 100 --width 350 \
 --title="RTL-AIS - Running." \
@@ -40,21 +49,29 @@ To stop, use this application and select \"Stop AIS capture and logging.\""
 }
 
 readto_db() {
-FILE=$(zenity --file-selection --title="RTL-AIS - Select File"
+FILE=$(zenity --file-selection --title="RTL-AIS - Select File" \
 --text="Select a file containing raw NMEA sentences to decode.");
 
 case $? in
 	0)
-		echo"\"$FILE\" selected.";;
+		echo "$FILE selected.";;
 	1)
-		echo"No file selected.";;
+		echo "No file selected."
+		WINDOW=$(zenity --info --height 100 --width 350 \
+		--title="RTL-AIS - NMEA - Decode to Database." \
+		--text="No file selected; no decoding accomplished.")
+		exit;;
 	2)
-		echo"Something went wrong...";;
+		echo "Something went wrong..."
+		WINDOW=$(zenity --info --height 100 --width 350 \
+		--title="RTL-AIS - NMEA - Decode to Database." \
+		--text="No file selected; no decoding accomplished.")
+		exit;;
 esac
 
 nmeafile=$FILE
 # call the python script to read the file and build the database
-mate-terminal -e "ais-fileto-sqlite $nmeafile"
+sh -c "ais-fileto-sqlite $nmeafile"
 
 WINDOW=$(zenity --info --height 100 --width 350 \
 --title="RTL-AIS - NMEA - Decode to Database." \
@@ -66,7 +83,7 @@ readto_json() {
 FILE=$(zenity --file-selection --title="RTL-AIS - Select File with NMEA Data");
 case $? in
 	0)
-		echo"\"$FILE\" selected."
+		echo "\"$FILE\" selected."
 		TIME=$(date +%Y-%m-%d_%H-%M-%S)
 		nmeafile=$FILE
 		# cat -> decode -> save
@@ -77,29 +94,56 @@ case $? in
 		--text="The decoder process has run; \"...-ais-decoded.json\" written.")
 		;;
 	1)
-		echo"No file selected."
+		echo "No file selected."
 		WINDOW=$(zenity --info --height 100 --width 350 \
 		--title="RTL-AIS - NMEA - Decode to JSON." \
 		--text="No file selected; no decoding accomplished.")
+		exit
 		;;
 	2)
-		echo"Something went wrong..."
+		echo "Something went wrong..."
 		WINDOW=$(zenity --info --height 100 --width 350 \
 		--title="RTL-AIS - NMEA - Decode to JSON." \
 		--text="Something went wrong; no decoding accomplished.")
+		exit
 		;;
 esac
 }
 
 startmapper() {
-WINDOW=$(zenity --info --height 100 --width 350 \
---title="RTL-AIS - Reserved." \
---text="This selection is reserved for future use"
-);
+# Select the json file to send to the mapper
+FILE=$(zenity --file-selection --title="RTL-AIS - Select Json File with AIS Data");
+case $? in
+	0)
+		echo "\"$FILE\" selected."
+		jsonfile=$FILE
+		ais-mapper $jsonfile
+
+		WINDOW=$(zenity --info --height 100 --width 350 \
+		--title="RTL-AIS - Mapper Starting." \
+		--text="Data has been sent to the mapper.")
+		;;
+	1)
+		echo "No file selected."
+		WINDOW=$(zenity --info --height 100 --width 350 \
+		--title="RTL-AIS - Mapping Failed: No File Selected" \
+		--text="No file selected; no map plotting possible.")
+		exit
+		;;
+	2)
+		echo "Something went wrong..."
+		WINDOW=$(zenity --info --height 100 --width 350 \
+		--title="RTL-AIS - Mapping Failed: Something Went Wrong" \
+		--text="Something went wrong; no mapping accomplished.")
+		exit
+		;;
+esac
+
 }
 
 stopcapture() {
 killall rtl_ais nc
+rm -f $fifo
 }
 
 ans=$(zenity  --list  --title "AIS MONITOR" --width=600 --height=280 \
@@ -109,7 +153,7 @@ FALSE "Capture AIS NMEA sentences by radio, decode, and write to a logfile."  \
 FALSE "Capture AIS NMEA sentences by radio, decode, and write to a database." \
 FALSE "Read a file containing AIS NMEA sentences, decode, and write to a database." \
 FALSE "Read a file containing AIS NMEA sentences, decode, and write to a json file." \
-FALSE "Plot AIS database info on a map." \
+FALSE "Plot decoded AIS info on a map." \
 TRUE "Stop AIS capture and logging.");
 
 	if [  "$ans" = "Capture AIS NMEA sentences by radio, decode, and write to a logfile." ]; then
@@ -124,7 +168,7 @@ TRUE "Stop AIS capture and logging.");
 	elif [  "$ans" = "Read a file containing AIS NMEA sentences, decode, and write to a json file." ]; then
 		readto_json
 
-	elif [  "$ans" = "Plot AIS database info on a map." ]; then
+	elif [  "$ans" = "Plot decoded AIS info on a map." ]; then
 		startmapper
 
 	elif [  "$ans" = "Stop AIS capture and logging." ]; then
