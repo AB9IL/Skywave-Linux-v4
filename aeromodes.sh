ac5f1c3ed95b56bbe73843f424a6ce14fe9d1eb7
Philip Colllier <redcrown01@gmail.com>
2022-03-05 13:41:30 +0000

    Update aeromodes.sh

--- aeromodes.sh
+++ aeromodes.sh
@@ -12,19 +12,31 @@
 #application must run one at a time on a device.
 
 #-------------Set Variables----------------------------------------
-#Designate up to eight ACARS frequencies, one per line, specified in MHz:
-readarray -t afreq < /usr/local/etc/ACARS_FREQS
-echo "Found the ACARS frequency list in ~/Documents/ACARS_FREQS: ${afreq[@]}"
-#Designate up to eight vdl mode 2 frequencies, one per line specified in MHz:
-readarray -t vfreq < /usr/local/etc/VDL_FREQS
-echo "Found the VDL frequency list in ~/Documents/VDL_FREQS: ${vfreq[@]}"
+# Set the path for the file with ACARS frequencies.
+# Designate up to eight ACARS frequencies,
+# one per line, specified in MHz.
+acarsfreqs="/usr/local/etc/ACARS_FREQS"
+
+# Set the path for the file with VDL Mode 2 frequencies.
+# Designate up to eight VDL Mode 2 frequencies,
+# one per line specified in MHz.
+vdl2freqs="/usr/local/etc/VDL_FREQS"
 
 #Specify the ACARS database file:
 acarslog="$HOME/acarsserv.db"
 #Specify the VDL2 database file:
 vdl2log="$HOME/vdl2serv.db"
 
+
+
 #-------------There be dragons below this line---------------------
+#Read the ACARS frequencies.
+readarray -t afreq < "$acarsfreqs"
+echo "Found the ACARS frequency list in ${acarsfreqs}: ${afreq[@]}"
+#
+readarray -t vfreq < /usr/local/etc/VDL_FREQS
+echo "Found the VDL frequency list in ${vdl2freqs}: ${vfreq[@]}"
+
 #Get the SDR frequency offset (ppm)
 ppm=$(cat /usr/local/etc/sdr_offset)
 #Get the SDR gain (gain)
@@ -36,7 +48,7 @@ key=$(echo $devkey | cut -f2 -d "=")
 
 zero="0"
 ans=$(zenity  --list  --title="RTLSDR Multichannel Digital Decoders" \
---height 450 --width 400 \
+--height 540 --width 400 \
 --text="ACARSdec and VDLM2dec functions:
 -- ACARSdec decodes ACARS
 -- VDLM2dec decodes VDL Mode 2
@@ -58,8 +70,8 @@ FALSE "Edit VDL Mode 2 Frequencies" \
 TRUE "Stop ACARSdec or VDLM2dec" \
 --column "Action");
 
-if [  "$ans" = "Run ACARSdec" ]; then
-	acarsdec -v -o 2 -j 127.0.0.1:5555 -g $gain$zero -p $ppm -r $key ${afreq[@]} &
+[[ "$ans" == "Run ACARSdec" ]] && \
+	(acarsdec -v -o 2 -j 127.0.0.1:5555 -g $gain$zero -p $ppm -r $key ${afreq[@]} &
 	sleep 3
 	acarsserv -v -j 127.0.0.1:5555 -b $acarslog -s &
 	sleep 3
@@ -67,10 +79,10 @@ if [  "$ans" = "Run ACARSdec" ]; then
 	WINDOW=$(zenity --info --height 100 --width 350 \
 	--title="ACARSdec - Running." \
 	--text="The multi channel ACARS monitor is running.
-To stop, use this application and select \"Stop ACARSdec.\"");
+        To stop, use this application and select \"Stop ACARSdec.\"");)
 
-elif [  "$ans" = "Run VDLM2dec" ]; then
-	vdlm2dec -v -J -G -E -j 127.0.0.1:5555 -g $gain$zero -p $ppm -r $key ${vfreq[@]} &
+[[ "$ans" == "Run VDLM2dec" ]] && \
+	(vdlm2dec -v -J -G -E -j 127.0.0.1:5555 -g $gain$zero -p $ppm -r $key ${vfreq[@]} &
 	sleep 3
 	acarsserv -v -j 127.0.0.1:5555 -b $vdl2log -s &
 	sleep 3
@@ -78,19 +90,15 @@ elif [  "$ans" = "Run VDLM2dec" ]; then
 	WINDOW=$(zenity --info --height 100 --width 350 \
 	--title="VDLM2dec - Running." \
 	--text="The multi channel VDLM2 monitor is running.
-To stop, use this application and select \"Stop VDLM2dec.\"");
+        To stop, use this application and select \"Stop VDLM2dec.\"");)
 
-elif [  "$ans" = "Edit ACARS Frequencies" ]; then
-	mate-terminal -e "nano /usr/local/etc/ACARS_FREQS"
+[[ "$ans" == "Edit ACARS Frequencies" ]] && \
+	x-terminal-emulator -e vim $acarsfreqs
 
-elif [  "$ans" = "Edit VDL Mode 2 Frequencies" ]; then
-	mate-terminal -e "nano /usr/local/etc/VDL_FREQS"
+[[ "$ans" == "Edit VDL Mode 2 Frequencies" ]] && \
+	x-terminal-emulator -e vim $vdl2freqs
 
-elif [  "$ans" = "Stop ACARSdec or VDLM2dec" ]; then
-	killall -9 vdlm2dec
-	killall -9 acarsdec
-	killall -9 acarsserv
-	killall -9 sqlitebrowser
-	exit
-
-fi
+[[ "$ans" == "Stop ACARSdec or VDLM2dec" ]] && \
+	(killall -9 vdlm2dec;
+    killall -9 acarsdec;
+    killall -9 acarsserv;)
